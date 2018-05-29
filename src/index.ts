2d8fffa53978593419fe730c094db97a07fa014a
Raúl Kripalani <raul.kripalani@consensys.net>
2018-05-29 22:00:31 +0100

    Add Jest as a test framework + initial tests.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import * as express from 'express';
-import * as graphqlHTTP from 'express-graphql';
-import { importSchema } from 'graphql-import';
-import { makeExecutableSchema } from 'graphql-tools';
-import * as _ from 'lodash';
-import resolvers from './resolvers';
+import schema from './schema';
+import { startServer } from './server';
 
-const buildSchema = () => {
-  const typeDefs = importSchema(__dirname + '/schema/index.graphql');
-  const schema = makeExecutableSchema({ typeDefs, resolvers });
-  return schema;
-};
-
-const app = express();
-app.use('/graphql', graphqlHTTP({ schema: buildSchema(), graphiql: true })).listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+startServer(schema);
