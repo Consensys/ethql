c8522aaddd6eee2caf167cedc0c8b410407f5958
Akhila Raju <akhila@berkeley.edu>
2018-06-18 11:19:51 -0400

    Fixes logic for 0 argument case in block to throw expected error and implements testing for 100% coverage.

--- src/__tests__/core/account.test.ts
+++ src/__tests__/core/account.test.ts
@@ -13,7 +13,6 @@ test('account: select by address', async () => {
   `;
 
   const expected = { data: { account: { code: '0x', transactionCount: 0 } } };
-
   const result = await execQuery(query);
   expect(result).toEqual(expected);
 });
@@ -31,4 +30,18 @@ test('account: error when address is invalid', async () => {
   const result = await execQuery(query);
   expect(result.errors).toHaveLength(1);
   expect(result.errors[0].message).toMatch(/^Expected type Address\!/);
+});
+
+test('account: select account balance', async () => {
+  const query = `
+    {
+      account(address: "0xF7c74BBC9b7e643193d46D9889538899321e5712") {
+        balance
+      }
+    }
+  `;
+
+  const expected = { data: { account: { balance: 201749298394112000 } } };
+  const result = await execQuery(query);
+  expect(result).toEqual(expected);
 });
