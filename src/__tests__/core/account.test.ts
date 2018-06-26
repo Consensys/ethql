f81cee6af5e248197d172ab9771c7b395c8363f5
Akhila Raju <akhila@berkeley.edu>
2018-06-26 14:41:56 -0400

    Support unit conversion for account balances.

--- src/__tests__/core/account.test.ts
+++ src/__tests__/core/account.test.ts
@@ -1,5 +1,4 @@
 import { testGraphql } from '../utils';
-
 const { execQuery } = testGraphql();
 
 test('account: select by address', async () => {
@@ -44,4 +43,32 @@ test('account: select account balance', async () => {
   const result = await execQuery(query);
   expect(result.errors).toBeUndefined();
   expect(result.data.account.balance).toBeGreaterThan(0);
+});
+
+test('account: select account balance with unit conversion', async () => {
+  const query = `
+    {
+      account(address: "0x0000000000000000000000000000000000000000") {
+        balance(unit: finney)
+      }
+    }
+  `;
+
+  const result = await execQuery(query);
+  expect(result.errors).toBeUndefined();
+  expect(result.data.account.balance).toBeGreaterThan(0);
+});
+
+test('account: error when unit is invalid', async () => {
+  const query = `
+    {
+      account(address: "0x0000000000000000000000000000000000000000") {
+        balance(unit: finey)
+      }
+    }
+  `;
+
+  const result = await execQuery(query);
+  expect(result.errors).toHaveLength(1);
+  expect(result.errors[0].message).toMatch('Expected type Unit, found finey; Did you mean the enum value finney?');
 });
--- src/model/core/EthqlAccount.ts
+++ src/model/core/EthqlAccount.ts
@@ -4,7 +4,16 @@ class EthqlAccount {
   constructor(public address: string) {}
 
   public async balance({ unit }, { web3 }: EthqlContext) {
-    return this.address && web3.eth.getBalance(this.address);
+    if (!this.address) {
+      return null;
+    }
+    // if (typeof unit !== 'string') { return 'Please enter a valid unit.'; }
+    const bal = await web3.eth.getBalance(this.address);
+    if (unit) {
+      return web3.utils.fromWei(bal, unit);
+    } else {
+      return bal;
+    }
   }
 
   public async code(_, { web3 }: EthqlContext) {
