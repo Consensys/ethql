960f11c2fd5ef2f6f0405912fc350f82d971965c
Akhila Raju <akhila.raju@consensys.net>
2018-07-05 15:43:52 -0400

    Introducing Parent->Block and Miner->Account queries. (#63)
    Implements ability to find the parent block of a given block and the miner account for a given block.

---
+++ src/__tests__/core/block.miner.test.ts
@@ -0,0 +1,28 @@
+import { testGraphql } from '../utils';
+
+const { execQuery } = testGraphql();
+
+test('block->miner: successfully returns miner account', async () => {
+  const query = `
+    {
+      block(number: 5771878) {
+        miner {
+          address
+          code
+        }
+      }
+    }`;
+
+  const expected = {
+    data: {
+      block: {
+        miner: {
+          address: '0x829BD824B016326A401d083B33D092293333A830',
+          code: '0x',
+        },
+      },
+    },
+  };
+  const result = await execQuery(query);
+  expect(result).toEqual(expected);
+});
