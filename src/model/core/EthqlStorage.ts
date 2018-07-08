e30235901bb191aa9a2a97122751d00ad7c31b04
jonathanBuhler <benedict.buhler@gmail.com>
2018-07-08 19:46:26 +0100

    Documented schema and code and general clean up
    Added notation for every query and documented any possibly obscure source code. Also changed netork back to mainnet from ropsten.

--- src/model/core/EthqlStorage.ts
+++ src/model/core/EthqlStorage.ts
@@ -2,30 +2,33 @@ import Web3 = require('web3');
 import { EthqlContext } from '../EthqlContext';
 
 interface PathObject {
-  storageType: string;
   query: string;
+  storageType: string;
 }
 
 class EthqlStorage {
-  public address: string;
-  public path: PathObject[];
-  public base: string;
-  public previousType: string;
+  private address: string;
+  private base: string;
+  private path: PathObject[];
+  private previousType: string;
 
   constructor(address: string) {
     this.address = address;
-    this.path = [];
     this.base = '';
+    this.path = [];
     this.previousType = '';
   }
 
+  //keyTypes supported: 'address', 'string', 'number'
   public solidityMap(args) {
     if (args.keyType.toLowerCase() === 'address') {
       this.addToPath(args.at, 'addressMap');
+    } else if (args.keyType.toLowerCase() === 'number') {
+      this.addToPath(args.at, 'numberMap');
     } else if (args.keyType.toLowerCase() === 'string') {
       this.addToPath(args.at, 'stringMap');
     } else {
-      this.addToPath(args.at, 'numberMap');
+      throw new TypeError('Not a supported keyType');
     }
 
     return this;
@@ -67,6 +70,8 @@ class EthqlStorage {
     this.previousType = type;
   }
 
+  //Depending on the storage type, solidity stores it in different locations
+  //These are the methods for the supported types
   private newBase(step: PathObject, web3: Web3) {
     if (step.storageType === 'numberMap' || step.storageType === 'addressMap') {
       return web3.utils.sha3('0x' + this.pad(step.query, web3) + this.pad(this.base, web3));
@@ -74,7 +79,7 @@ class EthqlStorage {
       return web3.utils.sha3(web3.utils.toHex(step.query) + this.pad(this.base, web3));
     } else if (step.storageType === 'fixedArray') {
       return web3.utils.toHex(web3.utils.toBN(this.base).add(web3.utils.toBN(step.query)));
-    } else {
+    } else if (step.storageType === 'dynamicArray') {
       return web3.utils.toHex(
         web3.utils.toBN(web3.utils.sha3('0x' + this.pad(this.base, web3))).add(web3.utils.toBN(step.query)),
       );
