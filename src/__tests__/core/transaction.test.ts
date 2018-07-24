59fd8c3e0bc4ad0c248a0d8ca6ada52fbe6c0ff0
Jonathan Buhler <32436504+jonathanBuhler@users.noreply.github.com>
2018-07-24 22:11:15 +0100

    Add Transaction->createdContract field (#88)
    Adds a Transaction->createdContract which returns an Account for the contract that was created by the transaction, if any. Also adds two test cases.

--- src/__tests__/core/transaction.test.ts
+++ src/__tests__/core/transaction.test.ts
@@ -124,3 +124,66 @@ test('transaction: status returns null when transaction status is undefined', as
   const result = await execQuery(query);
   expect(result).toEqual(expected);
 });
+
+test('transaction: createdContract returns data when "to" is null', async () => {
+  const query = `
+  {
+    transaction(hash: "0xb9c48f405d316996532fc5f14f8e7e687d2f859ba58c75543ca0161969c04ebf") {
+      to {
+        address
+      }
+      createdContract {
+        address
+        balance
+      }
+    }
+  }
+  `;
+
+  const expected = {
+    data: {
+      transaction: {
+        to: {
+          address: null,
+        },
+        createdContract: {
+          address: '0x7A83dB2d2737C240C77C7C5D8be8c2aD68f6FF23',
+          balance: 7247945487567546,
+        },
+      },
+    },
+  };
+
+  const result = await execQuery(query);
+  expect(result).toEqual(expected);
+});
+
+test('transaction: createdContract returns null when "to" is not null', async () => {
+  const query = `
+  {
+    transaction(hash: "0xdccbeb289f6630fd76fa2681837422fda9f76449653aa750d4e6b2822cf300fd") {
+      to {
+        address
+      }
+      createdContract {
+        address
+        balance
+      }
+    }
+  }
+  `;
+
+  const expected = {
+    data: {
+      transaction: {
+        to: {
+          address: '0x411A3811C366FD86461Cd1d6c559b1E6416fA810',
+        },
+        createdContract: null,
+      },
+    },
+  };
+
+  const result = await execQuery(query);
+  expect(result).toEqual(expected);
+});
