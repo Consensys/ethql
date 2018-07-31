754e82fabd24ca3ef485c3549a1d3a3e109d66cb
Raúl Kripalani <raul.kripalani@consensys.net>
2018-07-31 11:05:57 +0100

    (minor) Fix indentation.

--- src/__tests__/core/scalars.test.ts
+++ src/__tests__/core/scalars.test.ts
@@ -4,20 +4,20 @@ const { execQuery, execQueryWithVars, prepareContext } = testGraphql();
 
 test('Long: parseLiteral', async () => {
   const query = `
-      {
-        blockOffset(number: 12345, offset: 13212) {
-          number
-        }
+    {
+      blockOffset(number: 12345, offset: 13212) {
+        number
       }
-    `;
+    }
+  `;
 
-    const expected = { data: { blockOffset: { number: 25557 } } };
-    const result = await execQuery(query);
+  const expected = { data: { blockOffset: { number: 25557 } } };
+  const result = await execQuery(query);
   expect(result).toEqual(expected);
 });
 
 test('Long: parseValue', async () => {
-    const query = `
+  const query = `
     query($long: BlockNumber) {
       blockOffset(number: $long, offset: 13212) {
         number
@@ -26,19 +26,19 @@ test('Long: parseValue', async () => {
   `;
 
   const expected = { data: { blockOffset: { number: 25557 } } };
-  const vars = {'long': 12345};
+  const vars = { long: 12345 };
   const result = await execQueryWithVars(query, prepareContext(), vars);
   expect(result).toEqual(expected);
 });
 
 test('BlockNumber: parseLiteral', async () => {
   const query = `
-          {
-            block(number: 5958397) {
-                number
-              }
-            }
-        `;
+    {
+      block(number: 5958397) {
+          number
+        }
+      }
+  `;
 
   const expected = { data: { block: { number: 5958397 } } };
   const result = await execQuery(query);
@@ -47,15 +47,15 @@ test('BlockNumber: parseLiteral', async () => {
 
 test('BlockNumber: parseValue', async () => {
   const query = `
-      query($number: BlockNumber) {
-        block(number: $number) {
-            number
-          }
+    query($number: BlockNumber) {
+      block(number: $number) {
+          number
         }
-    `;
+      }
+  `;
 
   const expected = { data: { block: { number: 5958397 } } };
-  const vars = {'number': 5958397};
+  const vars = { number: 5958397 };
   const result = await execQueryWithVars(query, prepareContext(), vars);
   expect(result).toEqual(expected);
 });
@@ -64,9 +64,9 @@ test('Address: parseLiteral', async () => {
   const query = `
     {
       account(address: "0x0000000000000000000000000000000000000001") {
-          address
-        }
+        address
       }
+    }
   `;
 
   const expected = { data: { account: { address: '0x0000000000000000000000000000000000000001' } } };
@@ -76,27 +76,27 @@ test('Address: parseLiteral', async () => {
 
 test('Address: parseValue', async () => {
   const query = `
-          query($address: Address!) {
-            account(address: $address) {
-                address
-              }
-            }
-        `;
+    query($address: Address!) {
+      account(address: $address) {
+        address
+      }
+    }
+  `;
 
   const expected = { data: { account: { address: '0x0000000000000000000000000000000000000001' } } };
-  const vars = {'address': '0x0000000000000000000000000000000000000001'};
+  const vars = { address: '0x0000000000000000000000000000000000000001' };
   const result = await execQueryWithVars(query, prepareContext(), vars);
   expect(result).toEqual(expected);
 });
 
 test('Bytes32: parseLiteral', async () => {
   const query = `
-        {
-          block(hash: "0x4058f2f11f7399056d64e27e17de83ce564ce8f8dc6190f87473c8d456f45087") {
-              number
-            }
-          }
-      `;
+    {
+      block(hash: "0x4058f2f11f7399056d64e27e17de83ce564ce8f8dc6190f87473c8d456f45087") {
+        number
+      }
+    }
+  `;
 
   const expected = { data: { block: { number: 5958397 } } };
   const result = await execQuery(query);
@@ -105,15 +105,15 @@ test('Bytes32: parseLiteral', async () => {
 
 test('Bytes32: parseValue', async () => {
   const query = `
-        query($hash: Bytes32) {
-          block(hash: $hash) {
-            number
-          }
-        }
-      `;
+    query($hash: Bytes32) {
+      block(hash: $hash) {
+        number
+      }
+    }
+  `;
 
   const expected = { data: { block: { number: 5958397 } } };
-  const vars = {'hash': '0x4058f2f11f7399056d64e27e17de83ce564ce8f8dc6190f87473c8d456f45087'};
+  const vars = { hash: '0x4058f2f11f7399056d64e27e17de83ce564ce8f8dc6190f87473c8d456f45087' };
   const result = await execQueryWithVars(query, prepareContext(), vars);
   expect(result).toEqual(expected);
 });
