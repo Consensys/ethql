bf7969693e66ced2027f07f36ae9969dd7e865cb
Raúl Kripalani <raul.kripalani@consensys.net>
2018-08-01 16:14:30 +0100

    Encapsulate server behind EthqlServer class (+).
    * Pre-load an example query in Playground.
    * Remove package-lock.json

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,16 +1,29 @@
 import axios from 'axios';
 import * as net from 'net';
-import { getInfo, startServer, stopServer } from '../server';
+import { EthqlServer } from '../server';
 import { testGraphql, TestMode } from './utils';
 
+const testServers: EthqlServer[] = [];
+
 afterEach(async () => {
-  await stopServer();
+  console.log('Shutting down test servers...');
+  await testServers.forEach(async server => {
+    try {
+      await server.stop();
+    } catch (e) {
+      // do nothing
+    }
+  });
+  testServers.length = 0;
+  console.log('Test servers shut down.');
 });
 
 const healthCheckOk = async () => {
-  const port = getInfo().port;
-  const resp = await axios.post(`http://localhost:${port}/graphql`, { query: '{ health }' });
-  expect(resp.data).toEqual({ data: { health: 'ok' } });
+  testServers.forEach(async server => {
+    const port = server.address.port;
+    const resp = await axios.post(`http://localhost:${port}/graphql`, { query: '{ health }' });
+    expect(resp.data).toEqual({ data: { health: 'ok' } });
+  });
 };
 
 const availablePort = async () => {
@@ -25,94 +38,114 @@ test('server starts with random port', async () => {
   const { schema, ctxFactory } = testGraphql();
   ctxFactory.config.port = 0;
 
-  await startServer(schema, ctxFactory);
+  const server = new EthqlServer({ schema, ctxFactory });
+  testServers.push(server);
+  await server.start();
 
-  expect(getInfo().port).toBeGreaterThan(0);
+  expect(server.address.port).toBeGreaterThan(0);
+  await healthCheckOk();
 });
 
 test('server starts with an available port', async () => {
   const { schema, ctxFactory } = testGraphql();
   ctxFactory.config.port = await availablePort();
 
-  await startServer(schema, ctxFactory);
+  const server = new EthqlServer({ schema, ctxFactory });
+  testServers.push(server);
+  await server.start();
 
-  expect(getInfo().port).toBe(ctxFactory.config.port);
-
+  expect(server.address.port).toBe(ctxFactory.config.port);
   await healthCheckOk();
 });
 
-test('start twice does nothing on second attempt', async () => {
+test('start twice throws on second attempt', async () => {
   const { schema, ctxFactory } = testGraphql();
   ctxFactory.config.port = 0;
 
-  await startServer(schema, ctxFactory);
-  await startServer(schema, ctxFactory);
+  const server = new EthqlServer({ schema, ctxFactory });
+  testServers.push(server);
 
+  await server.start();
   await healthCheckOk();
-});
-
-test('stop twice does nothing on second attempt', async () => {
-  const { schema, ctxFactory } = testGraphql();
-  ctxFactory.config.port = 0;
-
-  await startServer(schema, ctxFactory);
-
-  const info = getInfo();
-  await healthCheckOk();
-
-  await stopServer();
-  await stopServer();
 
   try {
-    await axios.post(`http://localhost:${info.port}/graphql`, { query: '{ health }' });
-  } catch (err) {
+    await server.start();
+  } catch (e) {
+    expect(e).toBeInstanceOf(Error);
+    expect((e as Error).message).toMatch(/^Illegal EthQL server state/);
     return;
   }
+  fail('Expected when starting server twice exception');
+});
 
-  fail('expected a connection error as server should be closed');
+test('stop twice throws on second attempt', async () => {
+  const { schema, ctxFactory } = testGraphql();
+  ctxFactory.config.port = 0;
+
+  const server = new EthqlServer({ schema, ctxFactory });
+  testServers.push(server);
+
+  await server.start();
+  await healthCheckOk();
+  await server.stop();
+
+  try {
+    await server.stop();
+  } catch (e) {
+    expect(e).toBeInstanceOf(Error);
+    expect((e as Error).message).toMatch(/^Illegal EthQL server state/);
+    return;
+  }
+  fail('Expected when starting server twice exception');
 });
 
 test('server starts with random port', async () => {
   const { schema, ctxFactory } = testGraphql();
   ctxFactory.config.port = 0;
 
-  await startServer(schema, ctxFactory);
+  const server = new EthqlServer({ schema, ctxFactory });
+  testServers.push(server);
+  await server.start();
+  await healthCheckOk();
 
-  const info = getInfo();
-  expect(info.port).toBeGreaterThan(0);
+  expect(server.address.port).toBeGreaterThan(0);
 });
 
 test('JSON RPC endpoint configuration works correctly', async () => {
-  const ropsten = testGraphql({
-    configOverride: {
-      jsonrpc: 'https://ropsten.infura.io',
-      port: 0,
-    },
-    mode: TestMode.passthrough,
+  const servers = {
+    ropsten: new EthqlServer(
+      testGraphql({
+        configOverride: {
+          jsonrpc: 'https://ropsten.infura.io',
+          port: 0,
+        },
+        mode: TestMode.passthrough,
+      }),
+    ),
+    mainnet: new EthqlServer(
+      testGraphql({
+        configOverride: {
+          port: 0,
+        },
+        mode: TestMode.replay,
+      }),
+    ),
+  };
+
+  Object.values(servers).forEach(async s => {
+    testServers.push(s);
+    await s.start();
   });
 
-  await startServer(ropsten.schema, ropsten.ctxFactory);
+  await healthCheckOk();
 
-  let query = await axios.post(`http://localhost:${getInfo().port}/graphql`, {
-    query: '{ block(number: 1) { hash } }',
-  });
-  const ropstenHash = query.data.data.block.hash;
-
-  await stopServer();
-
-  const mainnet = testGraphql({
-    configOverride: {
-      port: 0,
-    },
-    mode: TestMode.replay,
+  const hashes = Object.values(servers).map(async s => {
+    const resp = await axios.post(`http://localhost:${s.address.port}/graphql`, {
+      query: '{ block(number: 1) { hash } }',
+    });
+    return resp.data.data.block.hash;
   });
 
-  await startServer(mainnet.schema, mainnet.ctxFactory);
-
-  query = await axios.post(`http://localhost:${getInfo().port}/graphql`, { query: '{ block(number: 1) { hash } }' });
-  const mainnetHash = query.data.data.block.hash;
-
-  expect(mainnetHash).not.toBeUndefined();
-  expect(ropstenHash).not.toBeUndefined();
-  expect(mainnetHash).not.toEqual(ropstenHash);
+  expect(hashes).toHaveLength(2);
+  expect(hashes[0]).not.toBe(hashes[1]);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,85 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, PlaygroundConfig } from 'apollo-server';
 import { GraphQLSchema } from 'graphql';
 import { AddressInfo } from 'net';
+import { WELCOME_QUERY } from './editor';
 import { EthqlContextFactory } from './model/EthqlContext';
 import EthqlQuery from './model/EthqlQuery';
 
-let server: ApolloServer;
-let info: AddressInfo;
+const playground: PlaygroundConfig = {
+  settings: { 'editor.theme': 'light' },
+  tabs: [
+    {
+      endpoint: '/',
+      query: WELCOME_QUERY,
+    },
+  ],
+};
 
-export async function startServer(schema: GraphQLSchema, ctxFactory: EthqlContextFactory) {
-  if (!server) {
-    server = new ApolloServer({
-      schema,
+type ServerStatus = 'stopped' | 'starting' | 'started' | 'stopping';
+
+/**
+ * Options for EthQL server.
+ */
+export type EthqlServerOpts = {
+  schema: GraphQLSchema;
+  ctxFactory: EthqlContextFactory;
+} & {
+  [prop: string]: any;
+};
+
+/**
+ * Represents an EthQL server currently using an Apollo Server as its backing implementation.
+ * In the future, this class could be made abstract or extracted to an interface if polymorphic
+ * server support is required.
+ */
+class EthqlServer {
+  private server: ApolloServer;
+  private _address: AddressInfo;
+  private _status: ServerStatus = 'stopped';
+
+  constructor(private opts: EthqlServerOpts) {}
+
+  public async start() {
+    if (this._status !== 'stopped') {
+      throw new Error(`Illegal EthQL server state upon start(). Expected status: stopped; actual: ${this._status}`);
+    }
+
+    this._status = 'starting';
+
+    const server = new ApolloServer({
+      schema: this.opts.schema,
       rootValue: new EthqlQuery(),
-      context: ctxFactory.create(),
+      context: () => this.opts.ctxFactory.create(),
+      playground,
     });
+
+    const { url, address, family, port } = await server.listen(this.opts.ctxFactory.config.port);
+    this.server = server;
+    this._address = { address, family, port: Number(port) };
+    this._status = 'started';
+
+    console.log(`🚀  Running a GraphQL API server at ${url}\nMerry querying!`);
   }
 
-  const { port } = ctxFactory.config;
-  await server.listen(port).then(({ url, address, family, port }) => {
-    info = { address, family, port: Number(port) };
-    console.log(`🚀 Server ready at ${url}, merry querying!`);
-  });
-}
+  public async stop() {
+    if (this._status !== 'started') {
+      throw new Error(`Illegal EthQL server state upon stop(). Expected status: started; actual: ${this._status}`);
+    }
 
-export async function stopServer() {
-  if (server) {
-    await server.stop();
-    info = null;
-    server = null;
+    this._status = 'stopping';
+
+    await this.server.stop();
+    this._address = this._status = this.server = null;
+    this._status = 'stopped';
+  }
+
+  public get address() {
+    return this._address;
+  }
+
+  public get status() {
+    return this._status;
   }
 }
 
-export function getInfo() {
-  return info;
-}
+export { EthqlServer };
